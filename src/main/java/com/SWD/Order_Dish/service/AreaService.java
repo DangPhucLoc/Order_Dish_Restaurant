5e2a5e2d976ba9408d599a3240394dc6ca0e2ad3
PhCh2k3 <phongdcse171753@fpt.edu.vn>
2024-06-05 23:55:04 +0700

    add LOGGER to AreaService

--- src/main/java/com/SWD/Order_Dish/service/AreaService.java
+++ src/main/java/com/SWD/Order_Dish/service/AreaService.java
@@ -2,10 +2,13 @@ package com.SWD.Order_Dish.service;
 
 import com.SWD.Order_Dish.entity.AreaEntity;
 import com.SWD.Order_Dish.entity.TableEntity;
+import com.SWD.Order_Dish.exception.CustomValidationException;
 import com.SWD.Order_Dish.model.area.AreaRequest;
 import com.SWD.Order_Dish.model.area.AreaResponse;
 import com.SWD.Order_Dish.repository.AreaRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -19,29 +22,41 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class AreaService {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(AreaService.class);
     private final AreaRepository areaRepository;
 
     public List<AreaResponse> findAll() {
+        LOGGER.info("Find all areas");
         List<AreaEntity> areas = areaRepository.findAll();
-        return !areas.isEmpty() ?
-                areas.stream()
-                        .map(this::areaResponseGenerator)
-                        .collect(Collectors.toList())
-                : null;
+        if (areas.isEmpty()) {
+            LOGGER.warn("No area was found!");
+        }
+        return areas.stream()
+                .map(this::areaResponseGenerator)
+                .collect(Collectors.toList());
     }
 
     public AreaResponse findById(String id) {
+        LOGGER.info("Find area with id " + id);
         Optional<AreaEntity> area = areaRepository.findById(id);
-        return area.map(this::areaResponseGenerator).orElse(null);
+        if (area.isEmpty()) {
+            LOGGER.warn("No area was found!");
+            return null;
+        }
+        return area.map(this::areaResponseGenerator).get();
     }
 
     public AreaResponse save(AreaRequest areaRequest) {
         AreaEntity area;
+
         if (areaRequest.getAreaId() != null) {
+            LOGGER.info("Update area with id " + areaRequest.getAreaId());
+            checkExist(areaRequest.getAreaId());
             area = areaRepository.findById(areaRequest.getAreaId()).get();
             updateArea(area, areaRequest);
             areaRepository.save(area);
         } else {
+            LOGGER.info("Create new area");
             area = createArea(areaRequest);
             areaRepository.save(area);
         }
@@ -50,6 +65,8 @@ public class AreaService {
 
     public void delete(String id) {
         if (id != null && !id.trim().isEmpty()) {
+            LOGGER.info("Delete area with id " + id);
+            checkExist(id);
             AreaEntity area = areaRepository.findById(id).get();
             areaRepository.delete(area);
         }
@@ -57,7 +74,7 @@ public class AreaService {
 
     private AreaEntity createArea(AreaRequest request) {
         AreaEntity area = new AreaEntity();
-        setCommonFields(area, request);
+        updateArea(area, request);
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         area.setCreatedBy(authentication.getName());
         area.setCreatedDate(new Date());
@@ -65,10 +82,6 @@ public class AreaService {
     }
 
     private void updateArea(AreaEntity area, AreaRequest request) {
-        setCommonFields(area, request);
-    }
-
-    private void setCommonFields(AreaEntity area, AreaRequest request) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         area.setName(request.getName());
         area.setIsAvailable(request.getIsAvailable());
@@ -91,5 +104,12 @@ public class AreaService {
                         .map(TableEntity::getTableId)
                         .collect(Collectors.toList()));
         return areaResponse;
+    }
+
+    private void checkExist(String id) {
+        if (areaRepository.findById(id).isEmpty()) {
+            LOGGER.error("No area was found!");
+            throw new CustomValidationException(List.of("No area was found!"));
+        }
     }
 }
