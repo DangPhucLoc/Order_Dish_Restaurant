47082aa4c5da41978a9248d254c8bc4d78797b91
Phuc Loc <locdpse171702@fpt.edu.vn>
2024-06-10 13:28:44 +0700

    add s3Util and lock the keyaccess

---
+++ src/main/java/com/SWD/Order_Dish/config/S3config.java
@@ -0,0 +1,29 @@
+package com.SWD.Order_Dish.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3config {
+    @Value("${aws.accessKeyId}")
+    private String awsAccessKey;
+
+    @Value("${aws.secretKey}")
+    private String awsSecretKey;
+
+    @Value("${aws.region}")
+    private String region;
+    @Bean
+    public AmazonS3 s3() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
